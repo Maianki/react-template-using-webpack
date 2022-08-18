4b2a9afe2226af98fc6e5362c6939619ee23b81e
Ankit Kumain <kumankit10@gmail.com>
2022-08-18 15:20:55 +0530

    feat: add styles and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "./style.css"
+import box from "./box.png"
+import winner from "./winner.svg"
+
 export default function App(){
     return(
-        <h1>React Typescript template by Ankit Kumain</h1>
+        <>
+            <h1>React Typescript template by Ankit Kumain</h1>
+            <div className="assets">
+                <img src={box} alt="open box"></img>
+                <img src={winner} alt="win poster"></img>
+            </div>
+        </>   
     )
 }
