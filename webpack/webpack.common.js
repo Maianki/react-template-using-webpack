17d556cae31f7602ec996a518263051b4fcea3fe
Ankit Kumain <kumankit10@gmail.com>
2022-08-24 22:55:06 +0530

    feat: add dev and production setup

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -31,7 +31,6 @@ module.exports = {
     path: path.resolve(__dirname, "..", "./build"),
     filename: "bundle.js",
   },
-  mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
